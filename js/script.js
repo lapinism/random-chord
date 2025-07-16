6df94a04efd00cd2ab1f89d7a242d6b2bbe45c68
lapinism <kgangho00@gmail.com>
2025-07-16 14:34:06 +0900

    now it's usable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const welcome = document.querySelector('form');
 const content = document.querySelector('main');
 const curChord = document.querySelector('h2');
+const nextChord = document.querySelector('h3');
 
-const chordButton = [
+const noteButton = [
     document.querySelector('#one'),
     document.querySelector('#two'),
     document.querySelector('#three'),
@@ -11,29 +12,34 @@ const chordButton = [
     document.querySelector('#six'),
     document.querySelector('#seven'),
 ];
-const dominantSeventh = document.querySelector("#dominantSeventh");
-const majorSeventh = document.querySelector("#majorSeventh");
-const minorSeventh = document.querySelector("#minorSeventh");
+const flatButton = document.querySelector('#flat');
+const sharpButton = document.querySelector('#sharp');
+const dominantSeventh = document.querySelector('#dominantSeventh');
+const majorSeventh = document.querySelector('#majorSeventh');
+const minorSeventh = document.querySelector('#minorSeventh');
+const tempo = document.querySelector('#tempo');
 const chordName = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
 
 let chordList = [];
+let chordQueue = [];
 let curIntervalID = 0;
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+})
 
 function welcomeButton() {
     chordList = [];
     for (let i = 0; i < 7; i++) {
-        if (!chordButton[i].checked) {
+        if (!noteButton[i].checked) {
             continue;
         }
-        chordList.push(chordName[i]);
-        if (dominantSeventh.checked) {
-            chordList.push(chordName[i] + '7');
+        addChord(chordName[i]);
+        if (flatButton.checked) {
+            addChord(chordName[i] + 'b');
         }
-        if (majorSeventh.checked) {
-            chordList.push(chordName[i] + 'M7');
-        }
-        if (minorSeventh.checked) {
-            chordList.push(chordName[i] + 'm7');
+        if (sharpButton.checked) {
+            addChord(chordName[i] + '#');
         }
     }
     if (chordList.length < 2) {
@@ -42,8 +48,14 @@ function welcomeButton() {
     }
     welcome.style.display = 'none';
     content.style.display = 'flex';
-    updateChord();
-    curIntervalID = setInterval(updateChord, 1000);
+    
+    // init queue
+    chordQueue= [];
+    chordQueue.push(getNextChord());
+    chordQueue.push(getNextChord());
+    updateQueue();
+
+    curIntervalID = setInterval(updateQueue, 60 * 1000 / tempo.value);
 }
 
 function contentButton() {
@@ -52,10 +64,31 @@ function contentButton() {
     content.style.display = 'none';
 }
 
-function updateChord() {
+function addChord(note) {
+    chordList.push(note);
+    if (dominantSeventh.checked) {
+        chordList.push(note + '7');
+    }
+    if (majorSeventh.checked) {
+        chordList.push(note + 'M7');
+    }
+    if (minorSeventh.checked) {
+        chordList.push(note + 'm7');
+    }
+}
+
+
+function updateQueue() {
+    chordQueue.push(getNextChord());
+    chordQueue.shift();
+    curChord.innerText = chordQueue[0];
+    nextChord.innerText = chordQueue[1];
+}
+
+function getNextChord() {
     let nextChord = chordList[parseInt(Math.random() * chordList.length)];
-    while (curChord.innerText == nextChord) {
+    while (chordQueue.length > 0 && chordQueue[chordQueue.length - 1] == nextChord) {
         nextChord = chordList[parseInt(Math.random() * chordList.length)];
     }
-    curChord.innerText = nextChord;
+    return nextChord;
 }
